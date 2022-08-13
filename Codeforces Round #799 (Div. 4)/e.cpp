c4eb6096bdea9b47fba102cee35b7726d1f073b6
VICKY <vishujoshi21nov2000@gmail.com>
2022-08-13 12:02:07 +0530

    E Upsolved

--- Codeforces Round #799 (Div. 4)/e.cpp
+++ Codeforces Round #799 (Div. 4)/e.cpp
@@ -2,71 +2,47 @@
 using namespace std;
 #define fast                          \
     std::ios::sync_with_stdio(false); \
-    cin.tie(nullptr);
-#define endl '\n';
+    cin.tie(NULL);
 
 int main()
 {
-    // KNOCKCAT
-
     fast;
-    int tt;
-    cin >> tt;
-
-    while (tt--)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        int n, sum;
-        cin >> n >> sum;
-        vector<int> v(n);
-        vector<int> ovs, zvs;
+        int i = 0;
+        int j = 0;
 
-        for (int i = 0; i < n; ++i)
-        {
-            cin >> v[i];
-            if (v[i] == 0)
-                zvs.push_back(i);
-            else
-                ovs.push_back(i);
-        }
+        int n, s;
+        cin >> n >> s;
 
-        int s = accumulate(v.begin(), v.end(), 0);
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
 
-        if (s == sum)
+        int ans = INT_MAX;
+        int sum = 0;
+        while (i < n)
         {
-            cout << 0 << endl;
-        }
-        else if (s < sum)
-        {
-            cout << -1 << endl;
-        }
-        else
-        {
-            int i = 0, j = n - 1;
-            int k = 0, l = ovs.size() - 1;
-            int count = 0;
-            while (i < j)
+            sum += a[i];
+
+            while (j < n and sum > s)
             {
-                if (sum == s)
-                    break;
-                int mn = ovs[k] - i + 1, mx = j - ovs[l] + 1;
-
-                if (mn < mx)
-                {
-                    count += mn;
-                    i += mn;
-                    ++k;
-                }
-                else
-                {
-                    count += mx;
-                    j -= mx;
-                    --l;
-                }
-                s -= 1;
-                // cout<<ovs[k] << " "<< i<<" "<<ovs[j] << " " << j <<" " <<mn<<" "<<mx<<" ";
-                // cout<<"st "<<count<<endl;
+                sum -= a[j];
+                j++;
             }
-            cout << count << endl;
+
+            if (s == sum)
+            {
+                ans = min(ans, j + (n - (i + 1)));
+            }
+            i++;
         }
+
+        if (ans == INT_MAX)
+            cout << -1 << endl;
+        else
+            cout << ans << endl;
     }
 }
