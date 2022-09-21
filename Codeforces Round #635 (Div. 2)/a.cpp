04bb3fb8fe232e7316d051e144f3b67cd80aeabe
VICKY <vishujoshi21nov2000@gmail.com>
2022-09-21 23:13:47 +0530

    A solved

---
+++ Codeforces Round #635 (Div. 2)/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define endl "\n"
+#define mod 1e97
+#define int long long int
+int32_t main()
+{
+    // KNOCKCAT
+
+    fast;
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        cout << b << " " << c << " " << c << endl;
+    }
+
+    return 0;
+}
