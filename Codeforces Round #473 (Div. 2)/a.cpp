217b8916cdc59ca180591aeb0f996b61341cc276
VICKY <vishujoshi21nov2000@gmail.com>
2022-09-23 23:53:33 +0530

    A Solved

---
+++ Codeforces Round #473 (Div. 2)/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define endl "\n"
+#define mod 1e97
+// #define int long long int
+int32_t main()
+{
+    // KNOCKCAT
+
+    fast;
+    // int tt;
+    // cin >> tt;
+
+    // while (tt--)
+    // {
+    long long int n;
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << "Mahmoud" << endl;
+    else
+        cout << "Ehab" << endl;
+    // }
+
+    return 0;
+}
