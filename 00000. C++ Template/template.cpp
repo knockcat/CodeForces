4ce101c380936da68ec4af15cdc6c14722a92b58
VICKY <vishujoshi21nov2000@gmail.com>
2022-10-08 21:26:40 +0530

    Added Template

---
+++ 00000. C++ Template/template.cpp
@@ -0,0 +1,33 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast std::ios::sync_with_stdio(false);cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+int32_t main()
+{
+    fast;
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+        cout << "KNOCKCAT THIS SIDE :)";
+    }
+    return 0;
+}
