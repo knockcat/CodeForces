157b50d47addfb5700aebda633d6d9ee8a645517
VICKY <vishujoshi21nov2000@gmail.com>
2022-11-07 22:45:40 +0530

    B Solved

---
+++ Codeforces Round #697 (Div. 3)/b.cpp
@@ -0,0 +1,44 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+int32_t main()
+{
+    fast;
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+
+        int n;
+        cin >> n;
+
+        int a = n % 2020;
+        int b = n / 2020;
+
+        if (a <= b)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
