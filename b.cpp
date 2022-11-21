c09bf9e3e3e38d269c33a4ee323d125860a86b9e
VICKY <vishujoshi21nov2000@gmail.com>
2022-11-21 23:35:22 +0530

    B Solved

---
+++ b.cpp
@@ -0,0 +1,45 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+int32_t main()
+{
+    fast;
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+
+        int n;
+        cin >> n ;
+        vector<int> v(n);
+        for(int i= 0;i<n; ++i)
+        {
+            cin >> v[i];
+        }
+
+        int rem = 10 - v.size();
+    }
+
+    return 0;
+}
