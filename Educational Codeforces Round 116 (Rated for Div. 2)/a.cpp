093f54c6d9a2cf152e7228e0b2c8b43362217b55
VICKY <vishujoshi21nov2000@gmail.com>
2022-11-24 22:26:46 +0530

    A Solved

---
+++ Educational Codeforces Round 116 (Rated for Div. 2)/a.cpp
@@ -0,0 +1,39 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+int32_t main()
+{
+    fast;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        // Knockcat
+        string str;
+        cin >> str;
+
+        if (str[0] != str[str.size() - 1])
+            str[0] = str[str.size() - 1];
+
+        cout << str << endl;
+    }
+    return 0;
+}
