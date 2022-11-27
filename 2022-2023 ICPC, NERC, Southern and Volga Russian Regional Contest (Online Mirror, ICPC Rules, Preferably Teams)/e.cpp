bab5b394e69e2eff6521ff583b94ed0869d22d41
VICKY <vishujoshi21nov2000@gmail.com>
2022-11-27 21:34:53 +0530

    E Solved

---
+++ 2022-2023 ICPC, NERC, Southern and Volga Russian Regional Contest (Online Mirror, ICPC Rules, Preferably Teams)/e.cpp
@@ -0,0 +1,40 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+int32_t main()
+{
+    fast;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        // Knockcat
+        int n, a, b;
+        cin >> n >> a >> b;
+        int res = 0;
+        if (a <= b)
+            res = ceil((double)n / (double)a);
+        else
+            res = 1;
+        cout << res << endl;
+    }
+    return 0;
+}
