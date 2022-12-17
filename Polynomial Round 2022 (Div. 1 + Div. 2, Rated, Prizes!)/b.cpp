b0a7c9e0bac8469afa46f9a4dc8d665c9eea1c58
VICKY <vishujoshi21nov2000@gmail.com>
2022-12-17 22:47:49 +0530

    B Solved

---
+++ Polynomial Round 2022 (Div. 1 + Div. 2, Rated, Prizes!)/b.cpp
@@ -0,0 +1,50 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+int32_t main()
+{
+    fast;
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+        int n, m, k;
+        cin >> n >> m >> k;
+
+        vector<int> v(m);
+        int maxi = 0;
+        for (int i = 0; i < m; ++i)
+        {
+            cin >> v[i];
+            maxi = max(maxi, v[i]);
+        }
+        vector<int> color(m + 1, k);
+
+        if (maxi > (n + k - 1) / k)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+    return 0;
+}
