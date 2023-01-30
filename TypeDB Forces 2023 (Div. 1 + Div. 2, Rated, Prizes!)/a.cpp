a750a8552b6e2565aaff27814ef4b6ae255cead6
VICKY <vishujoshi21nov2000@gmail.com>
2023-01-30 23:54:26 +0530

    A Solved

---
+++ TypeDB Forces 2023 (Div. 1 + Div. 2, Rated, Prizes!)/a.cpp
@@ -0,0 +1,40 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+int32_t main()
+{
+    fast;
+    ll tt;
+    cin >> tt;
+    while (tt--)
+    {
+        // Knockcat
+
+        int n;
+        cin >> n;
+
+        if (n & 1)
+            cout << -1 << endl;
+        else
+            cout << 1 << " " << n / 2 << endl;
+    }
+    return 0;
+}
