9e77a282a9b79125dc1bb91debab2e545337d43e
VICKY <vishujoshi21nov2000@gmail.com>
2023-02-05 23:04:19 +0530

    B Solved

--- VK Cup 2012 Qualification Round 1/b.cpp
+++ VK Cup 2012 Qualification Round 1/b.cpp
@@ -28,68 +28,23 @@ int32_t main()
     // {
     // }
 
-    int n;
+    int n, s, count[5] = {0};
     cin >> n;
-    int a = 0, b = 0, c = 0, d = 0, count = 0;
-    for (int i = 0; i < n; i++)
+    while (n--)
     {
-        int t;
-        cin >> t;
-        if (t == 1)
-        {
-            a++;
-        }
-        else if (t == 2)
-        {
-            b++;
-        }
-        else if (t == 3)
-        {
-            c++;
-        }
-        else
-        {
-            d++;
-        }
+        cin >> s;
+        count[s] += 1;
     }
-
-    count += d;
-
-    if (c < a)
+    int total = count[4] + count[3] + count[2] / 2;
+    count[1] -= count[3];
+    if (count[2] % 2 == 1)
     {
-        count += c;
-        a = a - c;
-        c = 0;
+        total += 1;
+        count[1] -= 2;
     }
-    else if (a <= c)
+    if (count[1] > 0)
     {
-        count += a;
-        c = c - a;
-        a = 0;
+        total += (count[1] + 3) / 4;
     }
-
-    if (c > 0)
-    {
-        count += c;
-        c = 0;
-    }
-
-    if (b > 0)
-    {
-        count += b / 2;
-        b = b % 2;
-    }
-
-    int left = a + (b * 2);
-    if (left <= 4 && left > 0)
-    {
-        count += 1;
-    }
-    else if (left % 4 != 0)
-    {
-        count += (left / 4) + 1;
-        count += left / 4;
-    }
-
-    cout << count << "\n";
+    cout << total;
 }
