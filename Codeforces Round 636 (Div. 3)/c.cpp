3b04d3540e9b18ad9dd1f3387d30f96d20f72ade
VICKY <vishujoshi21nov2000@gmail.com>
2023-04-22 22:25:58 +0530

    C Solved

---
+++ Codeforces Round 636 (Div. 3)/c.cpp
@@ -0,0 +1,70 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+// This is to print to correct decimal places
+// cout << fixed << setprecision(10) << ans << endl;
+
+int32_t main()
+{
+    fast;
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+
+        for (auto &itr : v)
+            cin >> itr;
+
+        auto sign = [&](int x)
+        {
+            if (x > 0)
+                return true;
+            return false;
+        };
+
+        ll sum = 0, ans = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            ll mx = v[i];
+            int j = i;
+            while (j < n and sign(v[i]) == sign(v[j]))
+            {
+                mx = max(mx, v[j]);
+                ++j;
+            }
+            i = j - 1;
+
+            ans += mx;
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}
