77ba17aae4c317bd20e55ae8e955136cd9e70435
VICKY <vishujoshi21nov2000@gmail.com>
2023-04-30 10:43:55 +0530

    A Solved

---
+++ Codeforces Round 869 Div. 2/a.cpp
@@ -0,0 +1,54 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+// This is to print to correct decimal places
+// cout << fixed << setprecision(10) << ans << endl;
+
+int32_t main()
+{
+    fast;
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<vector<char>> mat(n, vector<char>(k));
+
+        map<vector<char>, int> mp;
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < k; ++j)
+                cin >> mat[i][j];
+            ++mp[mat[i]];
+        }
+
+        cout << mp[mat[0]] << endl;
+    }
+
+    return 0;
+}
