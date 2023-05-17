8caa82edb4757d9746271af319b698aa022c14b1
VICKY <vishujoshi21nov2000@gmail.com>
2023-05-17 23:20:17 +0530

    C Solved

---
+++ Codeforces Round 702 (Div. 3)/c.cpp
@@ -0,0 +1,61 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+// This is to print to correct decimal places
+// cout << fixed << setprecision(10) << ans << endl;
+
+int32_t main()
+{
+    fast;
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+
+        ll n;
+        cin >> n;
+
+        set<ll> st;
+
+        for (int i = 1; i * i * i <= n; ++i)
+            st.insert(i * i * i);
+
+        bool ok = false;
+
+        for (int i = 1; i * i * i <= n; ++i)
+        {
+            if (st.count(n - (i * i * i)))
+            {
+                ok = true;
+                break;
+            }
+        }
+
+        if (ok)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
