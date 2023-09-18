4b019d9ea12854b173480b29f0679855e44e27f3
VICKY <vishujoshi21nov2000@gmail.com>
2023-09-19 01:23:17 +0530

    A Solved

---
+++ CodeTON Round 6 (Div. 1 + Div. 2, Rated, Prizes!)/a.cpp
@@ -0,0 +1,61 @@
+// JAI SHREE RAM
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = -1e9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+// This is to print to correct decimal places
+// cout << fixed << setprecision(10) << ans << endl;
+
+int32_t main()
+{
+    fast;
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+
+        int n, k, x;
+        cin >> n >> k >> x;
+
+        if (k > n or k > x + 1)
+            cout << -1 << endl;
+        else
+        {
+            int maxi = x;
+
+            if (maxi == k)
+                --maxi;
+
+            ll sum = 0;
+
+            for (int i = 0; i < k; ++i)
+                sum += i;
+
+            for (int i = k; i < n; ++i)
+                sum += maxi;
+
+            cout << sum << endl;
+        }
+    }
+    return 0;
+}
