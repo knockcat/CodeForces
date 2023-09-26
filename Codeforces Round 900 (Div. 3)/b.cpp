98530d1eb44e0baec537fd32d4016450935a7d26
VICKY <vishujoshi21nov2000@gmail.com>
2023-09-26 22:28:09 +0530

    B Solved

---
+++ Codeforces Round 900 (Div. 3)/b.cpp
@@ -0,0 +1,47 @@
+// JAI SHREE RAM
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = -1e9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+// This is to print to correct decimal places
+// cout << fixed << setprecision(10) << ans << endl;
+
+int32_t main()
+{
+    fast;
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+
+        int n;
+        cin >> n;
+
+        for (int i = 0; i < n; ++i)
+        {
+            cout << (i * 2) + 1 << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
