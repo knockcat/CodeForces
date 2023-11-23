afd63a558daa9e46c98221b867dfeeb0ec6d1f91
VICKY <vishujoshi21nov2000@gmail.com>
2023-11-23 18:36:27 +0530

    A Solved

---
+++ Codeforces Round 912 (Div. 2)/a.cpp
@@ -0,0 +1,57 @@
+// JAI SHREE RAM
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = -1e9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+// This is to print to correct decimal places
+// cout << fixed << setprecision(10) << ans << endl;
+
+// You better take more time but avoid penalties
+// Look at Constraints
+
+int32_t main()
+{
+    fast;
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+
+        int n, k;
+        cin >> n >> k;
+        vector<int> v(n);
+        for (auto &itr : v)
+            cin >> itr;
+
+        if (is_sorted(v.begin(), v.end()))
+            cout << "YES" << endl;
+        else
+        {
+            if (k >= 2)
+                cout << "YES" << endl;
+            else
+                cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
