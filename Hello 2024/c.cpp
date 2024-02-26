163722c2d67d3303b992637e6d56207292656af5
knockcat <vishujoshi21nov2000@gmail.com>
2024-02-26 21:12:49 +0530

    C Solved

---
+++ Hello 2024/c.cpp
@@ -0,0 +1,79 @@
+// JAI SHREE RAM
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = -1e9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+// This is to print to correct decimal places
+// cout << fixed << setprecision(10) << ans << endl;
+
+// You better take more time but avoid penalties
+// Look at Constraints
+
+// If I'll be good at programming she will be mine
+
+int32_t main()
+{
+    fast;
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+
+        for (auto &ele : v)
+            cin >> ele;
+
+        int low = -1, high = -1;
+
+        int penalty = 0;
+
+        for (auto &ele : v)
+        {
+            if (ele > max(low, high))
+            {
+                ++penalty;
+                low = ele;
+            }
+            else if (ele <= min(low, high))
+            {
+                low = ele;
+            }
+            else
+            {
+                high = ele;
+            }
+
+            if (low > high)
+                swap(low, high);
+
+            // cout << low << ' ' << high << endl;
+        }
+
+        cout << max(0ll, penalty - 2) << endl;
+    }
+    return 0;
+}
