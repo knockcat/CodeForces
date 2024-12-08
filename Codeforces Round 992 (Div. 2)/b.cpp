449fcf8e733796393bc34ea13a5b49eaf2a5ee5f
knockcat <vishujoshi21nov2000@gmail.com>
2024-12-08 23:12:03 +0530

    A, B Solved

---
+++ Codeforces Round 992 (Div. 2)/b.cpp
@@ -0,0 +1,64 @@
+// JAI SHREE RAM
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = -1e9;
+const ll maxn = 1e5 + 10;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+// This is to print to correct decimal places
+// cout << fixed << setprecision(10) << ans << endl;
+
+// You better take more time but avoid penalties
+// Look at Constraints
+
+// If I'll be good at programming she will be mine -> Success :D
+
+int32_t main()
+{
+    fast; // should be disabled for Interactive Problems
+
+    int tt;
+    cin >> tt;
+
+    while (tt--)
+    {
+        // knockcat
+
+        int n;
+        cin >> n;
+
+        if (n == 1)
+            cout << 1 << '\n';
+        else if (n == 2)
+            cout << 2 << '\n';
+        else
+        {
+            ll ans = 2;
+            ll val = 4;
+            while(val < n)
+            {
+                ++ans;
+                val = val * 2;
+                val += 2;
+            }
+   
+            cout<<ans<<endl;
+        }
+    }
+    return 0;
+}
